52e7c1b5eefe4223a9425df0068e5174adf9d86f
O'Brien <83289969+OBrien-reece@users.noreply.github.com>
2022-07-02 02:06:29 +0300

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -14,6 +14,7 @@
 int main(void)
 {
 	int digit1, digit2, digit3;
+
 	for (digit1 = 0; digit1 < 8; digit1++)
 	{
 		for (digit2 = digit1 + 1; digit2 < 9; digit2++)
@@ -25,12 +26,15 @@ int main(void)
 				putchar((digit3 % 10) + '0');
 
 				if (digit1 == 7 && digit2 == 8 && digit3 == 9)
-					continue;	
+					continue;
+				
 				putchar(',');
 				putchar(' ');
 			}
 		}
 	}
+
 	putchar('\n');
+
 	return (0);
 }
