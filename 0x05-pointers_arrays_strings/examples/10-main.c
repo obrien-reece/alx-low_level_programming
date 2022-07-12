b0812d9b93f3ba9691a695973e4f6ba1fe32fdbc
OBrien-reece <indeche.evans.o@gmail.com>
2022-07-12 12:37:49 -0700

    yesterdays work

---
+++ 0x05-pointers_arrays_strings/examples/10-main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - you can not modify a
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int a[5];
+	int b;
+	int c[5];
+
+	a = 0; /*nop*/
+	a = &b;/*nop*/
+	a = c;/*nop*/
+	return (0);
+}
+
